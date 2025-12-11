3dff3fde6d27a2533f6392a571be0b058c12d7bb
Zeeshan Nawaz Satti <133124081+Zeeshan1945@users.noreply.github.com>
2025-12-11 17:39:50 +0500

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function RootLayout() {
+  const theme = useColorScheme(); // "light" or "dark"
+
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        <StatusBar style="dark" />
+      <StatusBar style={theme === "dark" ? "light" : "dark"} backgroundColor={theme === "dark" ? "#000" : "#fff"} />
         <Stack
           screenOptions={{
             headerShown: false,
